2a4c62a3acfaa7572cd7da7f3dc97f3b8e81a93d
利剑斩无明 <lijianzhanwuming@163.com>
2016-07-07 14:16:44 +0800

    添加判断是否为微信浏览器

---
+++ Application/Common/Service/WxService.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: lijian
+ * Date: 16/7/7
+ * Time: 下午2:06
+ */
+
+namespace Common\Service;
+
+class WxService {
+
+    /**
+     * 判断是否为微信浏览器
+     * @return bool
+     */
+    public static function isWxExplorer(){
+        if ( strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false ) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+}
